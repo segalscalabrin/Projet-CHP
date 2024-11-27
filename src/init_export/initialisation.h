fd2edad0955bbd8a676086d12c404c8e8e52fae3
Laptop Segal Raguenes-Scalabrin <segal.scalabrin@gmail.com>
2024-11-27 15:03:50 +0100

    changement test_params

--- src/init_export/initialisation.h
+++ src/init_export/initialisation.h
@@ -1,5 +1,3 @@
 #include "include.h"
 
 void read_parameters(int cas, int Nx, int Ny, Parameters *params, Fonctions *fct);
-
-void check_enter_params(const string Cas, const string Nx, const string Ny);
--- test/test_params.h
+++ test/test_params.h
@@ -1,1 +1,3 @@
 #include "include.h"
+
+void check_enter_params(const string Cas, const string Nx, const string Ny);
